6a1bf38c85dcb3616e913866fca16942387ad599
Brayan Lujan Munoz <brayan.lujan@evertecinc.com>
2024-05-10 18:31:13 -0500

    Layout created like a component app-layout.blade.php

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -4,17 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 11 | Posts</title>
+    <title>Laravel 11</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
 
     {{-- fontawesome --}}
     {{-- tipografia --}}
+
 </head>
 <body>
 
     <header></header>
 
-    <h1>Welcome to the posts page</h1>
+    {{ $slot }}
 
     <footer></footer>
+
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 11</title>
+<!-- Whit this x-app-layout the component app-layout.blade.php is 
+    called and the code is puted in the '{ { $slot } }' variable -->
+<x-app-layout>
 
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body>
     <div class="max-w-4xl mx-auto px-4">
-        <x-alert2 type="awa" class="mb-4">
+        <x-alert2 type="info" class="mb-4">
             <x-slot name="titlel">
                 Titulo
             </x-slot>
@@ -20,5 +13,5 @@
 
         <p>Hola mundo</p>
     </div>
-</body>
-</html>
+
+</x-app-layout>

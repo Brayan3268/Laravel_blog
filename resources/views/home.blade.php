33733abbf0391a879eedf08fad9032af7a8db6a2
Brayan Lujan Munoz <brayan.lujan@evertecinc.com>
2024-05-10 18:44:56 -0500

    Layout created like a layout in folder named layouts - layout.blade.php and modified de home.blade.php for use this layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,37 @@
-<!-- Whit this x-app-layout the component app-layout.blade.php is 
-    called and the code is puted in the '{ { $slot } }' variable -->
-<x-app-layout>
+@extends('layouts.app')
 
+<!-- Other way for pass the title, because is just a one line of code -->
+@section('title', 'Laravel 11')
+
+@push('css')
+    <style>
+        body{
+            background-color: #f3f3f3
+        }
+    </style>
+@endpush
+
+@push('css')
+    <style>
+        body{
+            color: red
+        }
+    </style>
+@endpush
+
+@section('content')
+    
     <div class="max-w-4xl mx-auto px-4">
+
         <x-alert2 type="info" class="mb-4">
             <x-slot name="titlel">
                 Titulo
             </x-slot>
-            
+        
             Texto prueba variable slot
         </x-alert2>
 
         <p>Hola mundo</p>
     </div>
 
-</x-app-layout>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 11</title>
+    <title>@yield('title', 'Default value')</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
 
     {{-- fontawesome --}}
     {{-- tipografia --}}
+
+    @stack('css')
 
 </head>
 <body>
@@ -21,5 +23,6 @@
 
     <footer></footer>
 
+    @stack('js')
 </body>
 </html>
